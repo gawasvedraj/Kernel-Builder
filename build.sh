59668a87ad5fbea204198a6e01919de0649e46f7
Aqua <s24.burhan@gmail.com>
2024-05-31 18:00:41 +0530

    test

--- build.sh
+++ build.sh
@@ -151,7 +151,7 @@ function kernelsu() {
 # Enviromental variable
 DEVICE_MODEL="Redmi 9"
 DEVICE_CODENAME="lancelot"
-export DEVICE_DEFCONFIG="lancelot_defconfig"
+export DEVICE_DEFCONFIG="lava_defconfig"
 export ARCH="arm64"
 export KBUILD_BUILD_USER="Aqua"
 export KBUILD_BUILD_HOST="CI"
